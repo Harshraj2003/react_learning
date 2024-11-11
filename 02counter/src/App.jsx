fac2b763a0d04bd13fc4ca80a3bf42c9f412e5cf
Harshraj2003 <harshrajraj2003@gmail.com>
2024-11-12 02:06:51 +0530

    some new code added like components made

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,27 +4,101 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { useState } from 'react'
 
-/*import React from 'react'
+import React from 'react'
+import Card from './components/Card'
+
+
 
 const App = () => {
-
-  const user = "Harsh"
-  const age = 22
-
   return (
-    <div>Hello {user}, {age}</div>
+    <div>
+      <div className='p-10'>
+         <Card username="Harsh" idNo="A"/>
+         <Card username="Ayush" idNo="B"/>
+      </div>
+          
+
+    </div>
   )
 }
 
 export default App
- */
+
+
+
+
+/*
+      two way binding code 
+
+
+const App = () => {
+
+  const [username, setusername] = useState('')
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+
+    console.log(username)
+    
+    setusername('')
+
+  }
+  
+  return (
+    <div>
+        <form onSubmit={(e) => {
+          submitHandler(e)
+        }}> 
+          <input 
+             value ={username}
+              onChange={(e)=>{
+                setusername(e.target.value)
+              }}
+         className='py-4 px-3 text-xl m-5 rounded'type="text" placeholder='enter your name' />
+        <button className='py-4 px-3 text-xl font-semibold bg-emreld-600 rounded text-white'>submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App */
 
 
 
 
 
 
-function App() {
+
+
+
+
+
+
+
+
+
+
+
+/*const App = () => {
+  const [set, setA] = useState(0)
+
+
+  return (
+    <div>
+      <h3>Number is {set}</h3>
+      <button onClick={() =>setA(set + 5)}>Increment</button>
+      <button onClick={() =>setA(set - 5)}>decrement</button>
+
+    </div>
+  )
+}
+
+export default App */
+ 
+
+
+
+/*function App() {
   
   let [counter, setCounter] = useState(15)
 
@@ -50,4 +124,4 @@ function App() {
   )
 }
 
-export default App 
+export default App */
